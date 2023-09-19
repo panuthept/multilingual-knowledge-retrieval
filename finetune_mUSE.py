ddccf9c9a044f67eb7a39d37818d0f79ac308ef4
Panuthep <falcon_270394@hotmail.co.th>
2023-09-19 14:28:21 +0700

    update codes and commants

--- finetune_mUSE.py
+++ finetune_mUSE.py
@@ -11,8 +11,8 @@ import tensorflow_hub as hub
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Input
 from tensorflow.keras import backend as K
+from tensorflow.keras.utils import Sequence
 from tensorflow.keras.optimizers import AdamW
-from tensorflow.keras.utils import Sequence, SequenceEnqueuer
 
 
 def cal_mrr(targets, preds, *args, **kwargs):
@@ -56,7 +56,7 @@ class DPRDataset(Sequence):
                     data = json.loads(line.strip())
                     question = data["question"]
                     hard_negative_ids = data["top1000"]
-                    self.hard_negative_ids[question] = hard_negative_ids[:min(int(self.negative_size * self.hard_negative_factor), len(hard_negative_ids))]
+                    self.hard_negative_ids[question] = hard_negative_ids[:min(int(self.negative_size * self.hard_negative_factor), len(hard_negative_ids)) + 1]
 
         self.num_batches = len(self.qrels) // self.batch_size
         self.indices = list(range(len(self.qrels)))
@@ -116,8 +116,8 @@ class DPRDataset(Sequence):
                 negative_doc_ids = np.random.choice(negative_doc_ids, size=self.negative_size, replace=False)
                 negative_docs.extend([self.corpus[doc_id] for doc_id in negative_doc_ids])
             positive_doc = self.corpus[positive_doc_id]
-
             cand_docs.append([positive_doc] + negative_docs)
+
         labels = np.zeros((len(questions), self.negative_size + 1), dtype=np.int32)
         labels[:, 0] = 1
         return self.collate_fn(questions, cand_docs, labels)
@@ -131,24 +131,24 @@ def DPRTrainerTF(model, batch_size: int = 32, negative_size: int = 10):
         shape=[],
         batch_size=batch_size,
         dtype=tf.string
-    )
+    )   # (batch_size, )
     documents = Input(
         shape=[negative_size + 1],
         batch_size=batch_size,
         dtype=tf.string
-    )
+    )   # (batch_size, negative_size + 1)
 
-    flatten_documents = tf.reshape(documents, [-1])
+    flatten_documents = tf.reshape(documents, [-1]) # (batch_size x (negative_size + 1))
 
-    encoded_queries = model(queries)
-    encoded_documents = model(flatten_documents)
+    encoded_queries = model(queries)                # (batch_size, )
+    encoded_documents = model(flatten_documents)    # (batch_size x (negative_size + 1))
 
     # NOTE: We don't use in-batch negative because there are many questions that have the same positive document.
-    encoded_queries = tf.reshape(encoded_queries, [batch_size, 1, -1])
-    encoded_documents = tf.reshape(encoded_documents, [batch_size, negative_size + 1, -1])
+    encoded_queries = tf.reshape(encoded_queries, [batch_size, 1, -1])                      # (batch_size, 1, 512)
+    encoded_documents = tf.reshape(encoded_documents, [batch_size, negative_size + 1, -1])  # (batch_size, negative_size + 1, 512)
 
-    similarity_scores = tf.matmul(encoded_queries, encoded_documents, transpose_b=True)
-    similarity_scores = tf.reshape(similarity_scores, [batch_size, negative_size + 1])
+    similarity_scores = tf.matmul(encoded_queries, encoded_documents, transpose_b=True)     # (batch_size, 1, negative_size + 1)
+    similarity_scores = tf.reshape(similarity_scores, [batch_size, negative_size + 1])      # (batch_size, negative_size + 1)
     return Model(inputs=[queries, documents], outputs=similarity_scores)
 
 
@@ -156,6 +156,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--shuffle", action="store_true")
+    parser.add_argument("--verbose", action="store_true")
     parser.add_argument("--epochs", type=int, default=10)
     parser.add_argument("--workers", type=int, default=8)
     parser.add_argument("--learning_rate", type=float, default=0.001)
@@ -165,7 +166,8 @@ if __name__ == "__main__":
     parser.add_argument("--log_dir", type=str, default="logs")
     parser.add_argument("--save_dir", type=str, default="finetuned_models")
     parser.add_argument("--save_name", type=str, default="mUSE")
-    parser.add_argument("--negative_size", type=int, default=10)
+    parser.add_argument("--train_negative_size", type=int, default=10)
+    parser.add_argument("--val_negative_size", type=int, default=999)
     parser.add_argument("--hard_negative_factor", type=float, default=2.0)
     parser.add_argument("--validation_step", type=int, default=10)
     args = parser.parse_args()
@@ -177,7 +179,7 @@ if __name__ == "__main__":
         qrels_path=os.path.join(args.dataset_path, "qrel_train.jsonl"),
         batch_size=args.batch_size,
         shuffle=args.shuffle,
-        negative_size=args.negative_size,
+        negative_size=args.train_negative_size,
         hard_negative_path=os.path.join(args.dataset_path, "bm25_top1000.jsonl"),
         hard_negative_factor=args.hard_negative_factor,
     )
@@ -186,11 +188,11 @@ if __name__ == "__main__":
         validation_dataset = DPRDataset(
             corpus_path=os.path.join(args.dataset_path, "corpus.jsonl"), 
             qrels_path=os.path.join(args.dataset_path, "qrel_val.jsonl"),
-            batch_size=args.batch_size,
+            batch_size=1,
             shuffle=False,
-            negative_size=args.negative_size,
+            negative_size=args.val_negative_size,
             hard_negative_path=os.path.join(args.dataset_path, "bm25_top1000.jsonl"),
-            hard_negative_factor=args.hard_negative_factor,
+            hard_negative_factor=1.0,
         )
         print(f"Validation size: {len(validation_dataset)}")
 
@@ -202,69 +204,27 @@ if __name__ == "__main__":
         dtype=tf.string,
         trainable=True
     )
-    trainer = DPRTrainerTF(muse, args.batch_size, args.negative_size)
-    trainer.summary()
-    trainer.compile(optimizer=AdamW(learning_rate=args.learning_rate), loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True, axis=-1), metrics=cal_mrr)
+    print("Training model:")
+    training_model = DPRTrainerTF(muse, batch_size=args.batch_size, negative_size=args.train_negative_size)
+    training_model.summary()
+    training_model.compile(optimizer=AdamW(learning_rate=args.learning_rate), loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True, axis=-1), metrics=cal_mrr)
+    print("Validation model:")
+    validation_model = DPRTrainerTF(muse, batch_size=1, negative_size=args.val_negative_size)
+    validation_model.summary()
+    validation_model.compile(optimizer=AdamW(learning_rate=args.learning_rate), loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True, axis=-1), metrics=cal_mrr)
 
-    train_summary_writer = tf.summary.create_file_writer(args.log_dir)
+    summary_writer = tf.summary.create_file_writer(args.log_dir)
 
     step = 0
     best_score = -math.inf
-    for epoch_idx in trange(args.epochs, desc="Epoch"):
-        for train_batch_idx in trange(training_dataset.num_batches, desc="Training step"):
-            step += 1
-            # Training step
-            (queries, documents), labels = training_dataset[train_batch_idx]
-            train_outputs = trainer.train_on_batch(
-                x=[queries, documents],
-                y=labels,
-                return_dict=True,
-            )
-            train_loss = train_outputs["loss"]
-            train_mrr = train_outputs["cal_mrr"]
-            with train_summary_writer.as_default():
-                tf.summary.scalar("Train_Loss", train_loss, step)
-                tf.summary.scalar("Train_MRR", train_mrr, step)
-                tf.summary.scalar("Learning_Rate", K.get_value(trainer.optimizer.learning_rate), step)
 
-            # Validation step
-            if step % args.validation_step == 0:
-                if "validation_dataset" in locals():
-                    val_losses = []
-                    val_metrics = []
-                    for val_batch_idx in range(validation_dataset.num_batches):
-                        (queries, documents), labels = validation_dataset[val_batch_idx]
-                        val_outputs = trainer.test_on_batch(
-                            x=[queries, documents],
-                            y=labels,
-                            return_dict=True,
-                        )
-                        val_loss = val_outputs["loss"]
-                        val_mrr = val_outputs["cal_mrr"]
-                        val_losses.append(val_loss)
-                        val_metrics.append(val_mrr)
-                    val_losses = np.mean(val_losses)
-                    val_metrics = np.mean(val_metrics)
-                    with train_summary_writer.as_default():
-                        tf.summary.scalar("Valiation_Loss", val_losses, step)
-                        tf.summary.scalar("Valiation_MRR", val_metrics, step)
-                    if val_metrics > best_score:
-                        best_score = val_metrics
-                        tf.saved_model.save(hub_load, os.path.join(args.save_dir, args.save_name, "best_model"))
-                        with open(os.path.join(args.save_dir, args.save_name, "best_scores.txt"), "a") as f:
-                            f.write(f"{best_score} - {epoch_idx}\n")
-                else:
-                    tf.saved_model.save(hub_load, os.path.join(args.save_dir, args.save_name, "best_model"))
-        training_dataset.on_epoch_end()
-        K.set_value(trainer.optimizer.learning_rate, K.get_value(trainer.optimizer.learning_rate) * (1 - args.learning_rate_decay))
-
-    # Validation step
+    # Validation step (initial score)
     if "validation_dataset" in locals():
         val_losses = []
         val_metrics = []
-        for val_batch_idx in range(validation_dataset.num_batches):
+        for val_batch_idx in trange(validation_dataset.num_batches, disable=not args.verbose, desc="Validation step"):
             (queries, documents), labels = validation_dataset[val_batch_idx]
-            val_outputs = trainer.test_on_batch(
+            val_outputs = validation_model.test_on_batch(
                 x=[queries, documents],
                 y=labels,
                 return_dict=True,
@@ -275,7 +235,7 @@ if __name__ == "__main__":
             val_metrics.append(val_mrr)
         val_losses = np.mean(val_losses)
         val_metrics = np.mean(val_metrics)
-        with train_summary_writer.as_default():
+        with summary_writer.as_default():
             tf.summary.scalar("Valiation_Loss", val_losses, step)
             tf.summary.scalar("Valiation_MRR", val_metrics, step)
         if val_metrics > best_score:
@@ -283,5 +243,88 @@ if __name__ == "__main__":
             tf.saved_model.save(hub_load, os.path.join(args.save_dir, args.save_name, "best_model"))
             with open(os.path.join(args.save_dir, args.save_name, "best_scores.txt"), "a") as f:
                 f.write(f"{best_score}\n")
+        if args.verbose:
+            print(f"Valiation_Loss: {val_losses}, Valiation_MRR: {val_metrics}")
+    else:
+        tf.saved_model.save(hub_load, os.path.join(args.save_dir, args.save_name, "best_model"))
+
+    # Training loop
+    for epoch_idx in trange(args.epochs, desc="Epoch"):
+        for train_batch_idx in trange(training_dataset.num_batches, desc="Training step"):
+            step += 1
+            # Training step
+            (queries, documents), labels = training_dataset[train_batch_idx]
+            train_outputs = training_model.train_on_batch(
+                x=[queries, documents],
+                y=labels,
+                return_dict=True,
+            )
+            train_loss = train_outputs["loss"]
+            train_mrr = train_outputs["cal_mrr"]
+            with summary_writer.as_default():
+                tf.summary.scalar("Train_Loss", train_loss, step)
+                tf.summary.scalar("Train_MRR", train_mrr, step)
+                tf.summary.scalar("Learning_Rate", K.get_value(training_model.optimizer.learning_rate), step)
+
+            # Validation step
+            if step % args.validation_step == 0:
+                if "validation_dataset" in locals():
+                    val_losses = []
+                    val_metrics = []
+                    for val_batch_idx in trange(validation_dataset.num_batches, disable=not args.verbose, desc="Validation step"):
+                        (queries, documents), labels = validation_dataset[val_batch_idx]
+                        val_outputs = validation_model.test_on_batch(
+                            x=[queries, documents],
+                            y=labels,
+                            return_dict=True,
+                        )
+                        val_loss = val_outputs["loss"]
+                        val_mrr = val_outputs["cal_mrr"]
+                        val_losses.append(val_loss)
+                        val_metrics.append(val_mrr)
+                    val_losses = np.mean(val_losses)
+                    val_metrics = np.mean(val_metrics)
+                    with summary_writer.as_default():
+                        tf.summary.scalar("Valiation_Loss", val_losses, step)
+                        tf.summary.scalar("Valiation_MRR", val_metrics, step)
+                    if val_metrics > best_score:
+                        best_score = val_metrics
+                        tf.saved_model.save(hub_load, os.path.join(args.save_dir, args.save_name, "best_model"))
+                        with open(os.path.join(args.save_dir, args.save_name, "best_scores.txt"), "a") as f:
+                            f.write(f"{best_score} - {epoch_idx}\n")
+                    if args.verbose:
+                        print(f"Valiation_Loss: {val_losses}, Valiation_MRR: {val_metrics}")
+                else:
+                    tf.saved_model.save(hub_load, os.path.join(args.save_dir, args.save_name, "best_model"))
+        training_dataset.on_epoch_end()
+        K.set_value(training_model.optimizer.learning_rate, K.get_value(training_model.optimizer.learning_rate) * (1 - args.learning_rate_decay))
+
+    # Validation step
+    if "validation_dataset" in locals():
+        val_losses = []
+        val_metrics = []
+        for val_batch_idx in trange(validation_dataset.num_batches, disable=not args.verbose, desc="Validation step"):
+            (queries, documents), labels = validation_dataset[val_batch_idx]
+            val_outputs = validation_model.test_on_batch(
+                x=[queries, documents],
+                y=labels,
+                return_dict=True,
+            )
+            val_loss = val_outputs["loss"]
+            val_mrr = val_outputs["cal_mrr"]
+            val_losses.append(val_loss)
+            val_metrics.append(val_mrr)
+        val_losses = np.mean(val_losses)
+        val_metrics = np.mean(val_metrics)
+        with summary_writer.as_default():
+            tf.summary.scalar("Valiation_Loss", val_losses, step)
+            tf.summary.scalar("Valiation_MRR", val_metrics, step)
+        if val_metrics > best_score:
+            best_score = val_metrics
+            tf.saved_model.save(hub_load, os.path.join(args.save_dir, args.save_name, "best_model"))
+            with open(os.path.join(args.save_dir, args.save_name, "best_scores.txt"), "a") as f:
+                f.write(f"{best_score}\n")
+        if args.verbose:
+            print(f"Valiation_Loss: {val_losses}, Valiation_MRR: {val_metrics}")
     else:
         tf.saved_model.save(hub_load, os.path.join(args.save_dir, args.save_name, "best_model"))
