05134ba868089dd4a055e600f5c918301a47e404
Panuthep <falcon_270394@hotmail.co.th>
2023-09-24 22:44:40 +0700

    support batch_size argument and gpu check

--- eval_dense_retriever.py
+++ eval_dense_retriever.py
@@ -7,7 +7,18 @@ from mkr.retrievers.dense_retriever import DenseRetriever, DenseRetrieverConfig
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name", type=str, default="mUSE")
+    parser.add_argument("--batch_size", type=int, default=32)
     args = parser.parse_args()
+
+    # Check GPU available
+    if args.model_name == "mUSE":
+        # Use Tensorflow
+        import tensorflow as tf
+        print(f"GPU available: {tf.test.is_gpu_available()}")
+    else:
+        # Use PyTorch
+        import torch
+        print(f"GPU available: {torch.cuda.is_available()}")
 
     # Prepare retriever
     doc_retrieval = DenseRetriever(
@@ -16,10 +27,10 @@ if __name__ == "__main__":
             database_path=f"./database/{args.model_name}",
         ),
     )
-    doc_retrieval.add_corpus("iapp_wiki_qa", "./corpus/iapp_wiki_qa/corpus.jsonl")
-    doc_retrieval.add_corpus("tydiqa", "./corpus/tydiqa/corpus.jsonl")
-    doc_retrieval.add_corpus("xquad", "./corpus/xquad/corpus.jsonl")
-    doc_retrieval.add_corpus("miracl", "./corpus/miracl/corpus.jsonl")
+    doc_retrieval.add_corpus("iapp_wiki_qa", "./corpus/iapp_wiki_qa/corpus.jsonl", batch_size=args.batch_size)
+    doc_retrieval.add_corpus("tydiqa", "./corpus/tydiqa/corpus.jsonl", batch_size=args.batch_size)
+    doc_retrieval.add_corpus("xquad", "./corpus/xquad/corpus.jsonl", batch_size=args.batch_size)
+    doc_retrieval.add_corpus("miracl", "./corpus/miracl/corpus.jsonl", batch_size=args.batch_size)
 
     # Prepare benchmark
     benchmark = Benchmark(
